496cc9c947fd2fba7601117877022a4156c67a36
Doug Emery <doug@emeryit.com>
2013-01-16 11:53:15 -0500

    cleanup print messages; runnable from any dir, too

--- test/cleanup_test_manifests.sh
+++ test/cleanup_test_manifests.sh
@@ -3,8 +3,8 @@
 # delete the manifest files created by testing scripts
 MANIFEST_FILE=manifest-md5s.txt
 
+echo "removing test checksum files"
 while read dir
 do
-  echo "rm $dir/$MANIFEST_FILE"
-  rm $dir/$MANIFEST_FILE
+  rm -v `dirname $0`/../$dir/$MANIFEST_FILE
 done < `dirname $0`/dir_list_valid.txt
