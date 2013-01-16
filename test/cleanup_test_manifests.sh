8e91bb99d21b70078aea608be07c583a2dc7494a
Doug Emery <doug@emeryit.com>
2013-01-16 12:41:46 -0500

    cleanup check file exists for deleting

--- test/cleanup_test_manifests.sh
+++ test/cleanup_test_manifests.sh
@@ -6,5 +6,10 @@ MANIFEST_FILE=manifest-md5s.txt
 echo "removing test checksum files"
 while read dir
 do
-  rm -v `dirname $0`/../$dir/$MANIFEST_FILE
+  manifest=`dirname $0`/../$dir/$MANIFEST_FILE
+  if [ -f $manifest ]; then
+    rm -v $manifest
+  else
+    echo "not found $manifest"
+  fi
 done < `dirname $0`/dir_list_valid.txt
